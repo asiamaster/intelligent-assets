020c1a881a005bc58721c0d89c5ac238a24a87eb
shaofan <akhuting@hotmail.com>
2020-01-19 15:17:03 +0800

    初始化项目

---
+++ src/main/java/com/dili/intelligentassets/IntelligentAssetsApplication.java
@@ -0,0 +1,13 @@
+package com.dili.intelligentassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentAssetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentAssetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dili/intelligentassets/IntelligentAssetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dili.intelligentassets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentAssetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
