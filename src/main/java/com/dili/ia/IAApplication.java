e7cd225ae28d8c6125201bb9fabc1387fcd99f0c
kelan <qinkelan@diligrp.com>
2020-02-17 14:12:51 +0800

    uap  权限接入

--- src/main/java/com/dili/ia/IAApplication.java
+++ src/main/java/com/dili/ia/IAApplication.java
@@ -16,8 +16,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.dili.uap.dao", "com.dili.ia.mapper", "com.dili.ss.dao"})
-@ComponentScan(basePackages={"com.dili.ss", "com.dili.ia"})
-@RestfulScan({"com.dili.ia.rpc"})
+@ComponentScan(basePackages={"com.dili.ss", "com.dili.ia", "com.dili.uap.sdk"})
+@RestfulScan({"com.dili.ia.rpc","com.dili.uap.sdk.rpc"})
 @EnableDiscoveryClient
 @EnableFeignClients
 @DTOScan(value={"com.dili.ss", "com.dili.ia.domain"})
