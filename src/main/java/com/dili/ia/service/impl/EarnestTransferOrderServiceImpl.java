8444caf0432982d7ddcff3ddb0080cf029a2f82d
kelan <qinkelan@diligrp.com>
2020-03-09 17:10:19 +0800

    定金转移单字段调整，新增--code 编号，transfer_time定金转移成功时间。

--- src/main/java/com/dili/ia/service/impl/EarnestTransferOrderServiceImpl.java
+++ src/main/java/com/dili/ia/service/impl/EarnestTransferOrderServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 /**
  * 由MyBatis Generator工具自动生成
- * This file was generated on 2020-02-14 10:18:23.
+ * This file was generated on 2020-03-09 17:07:49.
  */
 @Service
 public class EarnestTransferOrderServiceImpl extends BaseServiceImpl<EarnestTransferOrder, Long> implements EarnestTransferOrderService {
