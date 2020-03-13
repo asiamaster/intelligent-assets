32d80e66c7c1358251c25a094cb668bb380fbf18
kelan <qinkelan@diligrp.com>
2020-03-13 16:16:24 +0800

    退款单打印数据加载接口

--- src/main/java/com/dili/ia/service/impl/RefundOrderDispatcherServiceImpl.java
+++ src/main/java/com/dili/ia/service/impl/RefundOrderDispatcherServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,15 +34,11 @@ public class RefundOrderDispatcherServiceImpl implements RefundOrderDispatcherSe
 
     @Autowired
     RefundOrderService refundOrderService;
-
-    @Lazy
+    @Autowired @Lazy
     private List<RefundOrderService> refundBizTypes;
     private Map<Integer,RefundOrderService> refundBiz = new HashMap<>();
     @PostConstruct
     public void init() {
-        if (CollectionUtils.isEmpty(refundBizTypes)){
-            return;
-        }
         for(RefundOrderService service : refundBizTypes) {
             for(Integer bizType : service.getBizType()) {
                 this.refundBiz.put(bizType, service);
