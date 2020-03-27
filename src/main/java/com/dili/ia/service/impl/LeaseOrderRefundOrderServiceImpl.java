1a721ad8f596724b28b808de81c9ab662b61d7ed
jiangchengyong <jiangchengyong@diligrp.com>
2020-03-27 10:58:54 +0800

    退款无处理方法实现 修改

--- src/main/java/com/dili/ia/service/impl/LeaseOrderRefundOrderServiceImpl.java
+++ src/main/java/com/dili/ia/service/impl/LeaseOrderRefundOrderServiceImpl.java
@@ -54,6 +54,16 @@ public class LeaseOrderRefundOrderServiceImpl extends BaseServiceImpl<RefundOrde
     }
 
     @Override
+    public BaseOutput submitHandler(RefundOrder refundOrder) {
+        return BaseOutput.success();
+    }
+
+    @Override
+    public BaseOutput withdrawHandler(RefundOrder refundOrder) {
+        return BaseOutput.success();
+    }
+
+    @Override
     public BaseOutput refundSuccessHandler(SettleOrder settleOrder, RefundOrder refundOrder) {
         try{
             return leaseOrderService.settleSuccessRefundOrderHandler(refundOrder);
