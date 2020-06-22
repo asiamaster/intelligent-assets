68e1d1af69f580e69658143ce663f771d1361946
qinkelan <qinkelan@diligrp.com>
2020-06-22 11:13:08 +0800

    删除冗余--保证金退款扣款单代码初始化--需求调整不要该功能

--- src/main/java/com/dili/ia/mapper/ChargebackOrderMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dili.ia.mapper;
-
-import com.dili.ia.domain.ChargebackOrder;
-import com.dili.ss.base.MyMapper;
-
-public interface ChargebackOrderMapper extends MyMapper<ChargebackOrder> {
-}
