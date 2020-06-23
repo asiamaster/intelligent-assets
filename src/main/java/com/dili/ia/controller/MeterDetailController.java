e3b809d82b685d8f89df5ac0af6862e3060d762c
xiaosa <202330gi>
2020-06-23 10:11:37 +0800

    新增：查看跳转、修改跳转、新增跳转

--- src/main/java/com/dili/ia/controller/MeterDetailController.java
+++ src/main/java/com/dili/ia/controller/MeterDetailController.java
@@ -1,9 +1,13 @@
 package com.dili.ia.controller;
 
 import com.dili.ia.domain.MeterDetail;
+import com.dili.ia.domain.dto.CustomerMeterDto;
 import com.dili.ia.service.MeterDetailService;
 import com.dili.ss.domain.BaseOutput;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -13,19 +17,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 由MyBatis Generator工具自动生成
- * This file was generated on 2020-06-12 11:35:07.
+ * @author:      xiaosa
+ * @date:        2020/6/23
+ * @version:     农批业务系统重构
+ * @description: 水电费
  */
 @Controller
 @RequestMapping("/meterDetail")
 public class MeterDetailController {
+
+    private final static Logger logger = LoggerFactory.getLogger(MeterDetailController.class);
+
     @Autowired
     MeterDetailService meterDetailService;
 
     /**
-     * 跳转到MeterDetail页面
-     * @param modelMap
-     * @return String
+     * @author:      xiaosa
+     * @date:        2020/6/23
+     * @param:
+     * @return:
+     * @description：跳转到欢迎页面
      */
     @RequestMapping(value="/index.html", method = RequestMethod.GET)
     public String index(ModelMap modelMap) {
@@ -33,14 +44,64 @@ public class MeterDetailController {
     }
 
     /**
-     * 分页查询MeterDetail，返回easyui分页信息
-     * @param meterDetail
-     * @return String
-     * @throws Exception
+     * @author:      xiaosa
+     * @date:        2020/6/23
+     * @param:
+     * @return:      
+     * @description：跳转到新增页面
+     */
+    @RequestMapping(value="/add.html", method = RequestMethod.GET)
+    public String add(ModelMap modelMap) {
+        return "meterDetail/add";
+    }
+
+
+    /**
+     * @author:      xiaosa
+     * @date:        2020/6/23
+     * @param:
+     * @return:
+     * @description：跳转到修改页面
+     */
+    @RequestMapping(value="/update.html", method = RequestMethod.GET)
+    public String update(ModelMap modelMap, Long id) {
+        MeterDetail meterDetail = null;
+        if (id != null) {
+            meterDetail = meterDetailService.get(id);
+        }
+        logger.info(meterDetail.toString());
+        modelMap.put("meterDetail", meterDetail);
+        return "meterDetail/update";
+    }
+
+    /**
+     * @author:      xiaosa
+     * @date:        2020/6/23
+     * @param:
+     * @return:
+     * @description：分页请求
      */
     @RequestMapping(value="/listPage.action", method = {RequestMethod.GET, RequestMethod.POST})
     public @ResponseBody String listPage(@ModelAttribute MeterDetail meterDetail) throws Exception {
         return meterDetailService.listEasyuiPageByExample(meterDetail, true).toString();
+    }
+
+    /**
+     * @author:      xiaosa
+     * @date:        2020/6/23
+     * @param:
+     * @return:
+     * @description：跳转到查看页面
+     */
+    @RequestMapping(value="/view.action", method = {RequestMethod.GET, RequestMethod.POST})
+    public String view(ModelMap modelMap, Long id) {
+        MeterDetail meterDetail = null;
+        if (id != null) {
+            meterDetail = meterDetailService.get(id);
+        }
+        logger.info(meterDetail.toString());
+        modelMap.put("meterDetail", meterDetail);
+        return "meterDetail/view";
     }
 
     /**
