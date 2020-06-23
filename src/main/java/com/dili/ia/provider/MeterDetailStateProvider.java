6acfa1f959b63423c45fb5526aaec0d8ef328f3a
xiaosa <202330gi>
2020-06-23 10:00:37 +0800

    新增：水电费单的状态的 provider 的相关添加

---
+++ src/main/java/com/dili/ia/provider/MeterDetailStateProvider.java
@@ -0,0 +1,49 @@
+package com.dili.ia.provider;
+
+import com.dili.ia.glossary.MeterDetailStateEnum;
+import com.dili.ia.glossary.MeterTypeEnum;
+import com.dili.ss.metadata.FieldMeta;
+import com.dili.ss.metadata.ValuePair;
+import com.dili.ss.metadata.ValuePairImpl;
+import com.dili.ss.metadata.ValueProvider;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author:      xiaosa
+ * @date:        2020/6/16
+ * @version:     农批业务系统重构
+ * @description: 水电费表类别的 provider
+ */
+@Component
+public class MeterDetailStateProvider implements ValueProvider {
+    private static final List<ValuePair<?>> BUFFER = new ArrayList<>();
+
+    static {
+        BUFFER.addAll(Stream.of(MeterDetailStateEnum.values())
+                .map(e -> new ValuePairImpl<>(e.getName(), e.getCode().toString()))
+                .collect(Collectors.toList()));
+    }
+
+    @Override
+    public List<ValuePair<?>> getLookupList(Object o, Map map, FieldMeta fieldMeta) {
+        return BUFFER;
+    }
+
+    @Override
+    public String getDisplayText(Object object, Map map, FieldMeta fieldMeta) {
+        if (null == object) {
+            return null;
+        }
+        ValuePair<?> valuePair = BUFFER.stream().filter(val -> object.toString().equals(val.getValue())).findFirst().orElseGet(null);
+        if (null != valuePair) {
+            return valuePair.getText();
+        }
+        return null;
+    }
+}
