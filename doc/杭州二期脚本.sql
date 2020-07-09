debc5a669f341ed3c6ae325373c2fbcf7fbfadf5
wangmi <87730066>
2020-07-09 16:17:20 +0800

    更新流程审批脚本

--- doc/杭州二期脚本.sql
+++ doc/杭州二期脚本.sql
@@ -42,3 +42,32 @@ DROP COLUMN `rent_refund_amount`;
 -- 改表名
 ALTER  TABLE lease_order RENAME TO assets_lease_order;
 ALTER  TABLE lease_order_item RENAME TO assets_lease_order_item;
+
+--租赁单和退款单新增流程实例和定义id
+ALTER TABLE `assets_lease_order`
+	ADD COLUMN `process_instance_id` VARCHAR(64) NULL DEFAULT NULL COMMENT '流程实例id' AFTER `version`,
+	ADD COLUMN `process_definition_id` VARCHAR(64) NULL DEFAULT NULL COMMENT '流程定义id' AFTER `process_instance_id`,
+	ADD COLUMN `approval_state` TINYINT NULL DEFAULT NULL COMMENT '审批状态(1: 待审批 2: 审批中 3:审批通过 4:审批拒绝)' AFTER `process_definition_id`;
+ALTER TABLE `refund_order`
+	ADD COLUMN `process_instance_id` VARCHAR(64) NULL DEFAULT NULL COMMENT '流程实例id' AFTER `version`,
+	ADD COLUMN `process_definition_id` VARCHAR(64) NULL DEFAULT NULL COMMENT '流程定义id' AFTER `process_instance_id`,
+    ADD COLUMN `approval_state` TINYINT NULL DEFAULT NULL COMMENT '审批状态(1: 待审批 2: 审批中 3:审批通过 4:审批拒绝)' AFTER `process_definition_id`;
+
+--创建审批表
+create table approval_process
+(
+   id                   bigint(20) not null comment 'id',
+   process_instance_id  varchar(64) comment '流程实例id',
+   task_name            varchar(50) comment '审批状态(根据流程来定义)',
+   task_id              varchar(64) comment '任务id(用于任务追踪)',
+   assignee             bigint comment '办理人id',
+   assignee_name        varchar(20) comment '办理人名称(冗余UAP用户名)',
+   create_time          datetime comment '创建时间',
+   opinion              varchar(120) comment '审批意见',
+   result               varchar(20) comment '审批结果',
+   business_key         varchar(20) comment '业务key',
+   business_type        tinyint comment '业务类型(1:租赁交费 2:租赁退款)',
+   process_name         varchar(40) comment '流程名称',
+   firm_id              bigint comment '商户id',
+   primary key (id)
+);
