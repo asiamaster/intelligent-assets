e04001cba50fb6fc8a66c7590b31cda7f39d2ca3
jiangchengyong <jiangchengyong@diligrp.com>
2020-07-20 17:09:09 +0800

    脚本加分号

--- doc/报表上线版本脚本.sql
+++ doc/报表上线版本脚本.sql
@@ -32,12 +32,12 @@ UPDATE
 	SET p.customer_id = alo.customer_id,
 	p.customer_name = alo.customer_name
 WHERE
-	p.biz_type = 1
+	p.biz_type = 1;
 	
 UPDATE payment_order po
 LEFT JOIN earnest_order eo ON po.business_id=eo.id
 SET po.customer_id=eo.customer_id, po.customer_name=eo.customer_name
-WHERE po.biz_type=2
+WHERE po.biz_type=2;
 
 
 -- ----------------------------
