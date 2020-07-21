b2c541145177195439e91341e7bc4688911ad584
yangfan <yangfan>
2020-07-21 17:36:59 +0800

    添加com.dili.bpmc.sdk扫描

--- src/main/java/com/dili/ia/IAApplication.java
+++ src/main/java/com/dili/ia/IAApplication.java
@@ -19,7 +19,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @EnableFeignClients(basePackages = {"com.dili.ia.rpc", "com.dili.logger.sdk.rpc", "com.dili.assets.sdk.rpc","com.dili.rule.sdk.rpc"})
 @MapperScan(basePackages = { "com.dili.ia.mapper", "com.dili.ss.dao"})
 @ComponentScan(basePackages={"com.dili.ss", "com.dili.ia", "com.dili.uap.sdk", "com.dili.logger.sdk", "com.dili.assets.sdk","com.dili.rule.sdk","com.dili.bpmc.sdk"})
-@RestfulScan({"com.dili.ia.rpc","com.dili.uap.sdk.rpc"})
+@RestfulScan({"com.dili.ia.rpc","com.dili.uap.sdk.rpc","com.dili.bpmc.sdk"})
 @DTOScan(value={"com.dili.ss", "com.dili.ia.domain", "com.dili.uap.sdk.domain"})
 public class IAApplication  extends SpringBootServletInitializer {
 
