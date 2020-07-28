bc305293580725ac22a46d0cc5a1ad39c85e9ea5
qinkelan <qinkelan@diligrp.com>
2020-07-28 15:32:23 +0800

    接口生成注释调整easy api接入

--- src/main/java/com/dili/ia/api/RefundOrderApi.java
+++ src/main/java/com/dili/ia/api/RefundOrderApi.java
@@ -15,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
+ * 退款单API
+ *
  * <B>Description</B>
  * 本软件源代码版权归农丰时代及其团队所有,未经许可不得任意复制与传播
  * <B>农丰时代科技有限公司</B>
@@ -33,7 +35,7 @@ public class RefundOrderApi {
 
     /**
      * 定金票据打印数据加载
-     * @param orderCode 订单号
+     * @param orderCode 退款单订单号CODE
      * @param reprint 是否补打标记
      * @return BaseOutput<PrintDataDto>
      */
@@ -53,8 +55,8 @@ public class RefundOrderApi {
 
     /**
      * 摊位租赁结算成功回调
-     * @param settleOrder
-     * @return
+     * @param settleOrder 结算单
+     * @return BaseOutput<Boolean>
      */
     @BusinessLogger(businessType="refund_order", content="${code!}", operationType="refund", systemCode = "INTELLIGENT_ASSETS")
     @RequestMapping(value="/settlementDealHandler", method = {RequestMethod.GET, RequestMethod.POST})
