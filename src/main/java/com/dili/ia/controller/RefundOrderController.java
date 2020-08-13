d95028893c4d8b4354355ae5c19c8eea89bad446
xiaosa <202330gi>
2020-08-13 15:42:05 +0800

    修改：精品停车、通行证查询退款详情页面修改

--- src/main/java/com/dili/ia/controller/RefundOrderController.java
+++ src/main/java/com/dili/ia/controller/RefundOrderController.java
@@ -171,9 +171,12 @@ public class RefundOrderController {
 
             if (refundOrder.getBizType().equals(BizTypeEnum.EARNEST.getCode())){
                 return "refundOrder/earnestRefundOrderView";
-            } else if (refundOrder.getBizType().equals(BizTypeEnum.DEPOSIT_ORDER.getCode()) || refundOrder.getBizType().equals(BizTypeEnum.BOUTIQUE_ENTRANCE.getCode())|| refundOrder.getBizType().equals(BizTypeEnum.PASSPORT.getCode())){
+            } else if (refundOrder.getBizType().equals(BizTypeEnum.DEPOSIT_ORDER.getCode())){
                 return "refundOrder/depositRefundOrderView";
-            }else if (refundOrder.getBizType().equals(BizTypeEnum.BOOTH_LEASE.getCode())){
+            } else if (refundOrder.getBizType().equals(BizTypeEnum.BOUTIQUE_ENTRANCE.getCode())|| refundOrder.getBizType().equals(BizTypeEnum.PASSPORT.getCode())){
+                return "refundOrder/commonRefundOrderView";
+            }
+            else if (refundOrder.getBizType().equals(BizTypeEnum.BOOTH_LEASE.getCode())){
                 TransferDeductionItem transferDeductionItemCondition = new TransferDeductionItem();
                 transferDeductionItemCondition.setRefundOrderId(id);
                 modelMap.put("transferDeductionItems",transferDeductionItemService.list(transferDeductionItemCondition));
