826607f3975815b923db99bcd7b22dae272cfd69
qinkelan <qinkelan@diligrp.com>
2020-08-22 14:51:17 +0800

    注释

--- src/main/java/com/dili/ia/service/DepositBalanceService.java
+++ src/main/java/com/dili/ia/service/DepositBalanceService.java
@@ -20,7 +20,7 @@ public interface DepositBalanceService extends BaseService<DepositBalance, Long>
     /**
      * 根据需求维度，查询客户余额， 精确查找 , 必要条件如下，如果不传，会按照 is null  方式查询！！！
      * @param depositBalance
-     * 注意： 只需及必填参数
+     * 注意： 只需及必填参数 如果参数为空  会按照 is null 方式查询
      * customer_id  客户ID
      * type_code 保证金类型code
      * assets_type 资产类型
