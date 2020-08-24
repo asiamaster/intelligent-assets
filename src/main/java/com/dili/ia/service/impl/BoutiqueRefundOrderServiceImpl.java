858e149385158e887b7af9ee0d63363d62ca3882
xiaosa <202330gi>
2020-08-24 17:14:43 +0800

    修改：精品停车、通行证退款成功后回调，转抵信息

--- src/main/java/com/dili/ia/service/impl/BoutiqueRefundOrderServiceImpl.java
+++ src/main/java/com/dili/ia/service/impl/BoutiqueRefundOrderServiceImpl.java
@@ -4,13 +4,16 @@ import com.dili.ia.domain.BoutiqueEntranceRecord;
 import com.dili.ia.domain.BoutiqueFeeOrder;
 import com.dili.ia.domain.RefundOrder;
 import com.dili.ia.domain.StockIn;
+import com.dili.ia.domain.TransferDeductionItem;
 import com.dili.ia.domain.dto.BoutiqueFeeOrderDto;
 import com.dili.ia.glossary.BizTypeEnum;
 import com.dili.ia.glossary.BoutiqueOrderStateEnum;
 import com.dili.ia.glossary.StockInStateEnum;
 import com.dili.ia.service.BoutiqueEntranceRecordService;
 import com.dili.ia.service.BoutiqueFeeOrderService;
+import com.dili.ia.service.CustomerAccountService;
 import com.dili.ia.service.RefundOrderDispatcherService;
+import com.dili.ia.service.TransferDeductionItemService;
 import com.dili.settlement.domain.SettleOrder;
 import com.dili.ss.base.BaseServiceImpl;
 import com.dili.ss.constant.ResultCode;
@@ -20,21 +23,34 @@ import com.dili.uap.sdk.domain.User;
 import com.dili.uap.sdk.domain.UserTicket;
 import com.dili.uap.sdk.session.SessionContext;
 import com.google.common.collect.Sets;
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 @Service
 public class BoutiqueRefundOrderServiceImpl extends BaseServiceImpl<RefundOrder, Long> implements RefundOrderDispatcherService {
 
+    private final static Logger logger = LoggerFactory.getLogger(BoutiqueRefundOrderServiceImpl.class);
+
+
     @Autowired
     private BoutiqueFeeOrderService boutiqueFeeOrderService;
 
     @Autowired
     private BoutiqueEntranceRecordService boutiqueEntranceRecordService;
+
+    @Autowired
+    private TransferDeductionItemService transferDeductionItemService;
+
+    @Autowired
+    private CustomerAccountService customerAccountService;
 
     /**
      * 退款单 -- 提交(退款的提交无需改变通行证缴费单的信息)
@@ -76,6 +92,22 @@ public class BoutiqueRefundOrderServiceImpl extends BaseServiceImpl<RefundOrder,
         }
 
         this.updateState(refundOrder.getBusinessCode(), orderDto.getVersion(), BoutiqueOrderStateEnum.REFUNDED);
+
+        //转抵扣充值
+        TransferDeductionItem transferDeductionItemCondition = new TransferDeductionItem();
+        transferDeductionItemCondition.setRefundOrderId(refundOrder.getId());
+        List<TransferDeductionItem> transferDeductionItems = transferDeductionItemService.list(transferDeductionItemCondition);
+        if (CollectionUtils.isNotEmpty(transferDeductionItems)) {
+            transferDeductionItems.forEach(o -> {
+                BaseOutput accountOutput = customerAccountService.leaseOrderRechargTransfer(
+                        refundOrder.getId(), refundOrder.getCode(), o.getPayeeId(), o.getPayeeAmount(),
+                        refundOrder.getMarketId(), refundOrder.getRefundOperatorId(), refundOrder.getRefundOperator());
+                if (!accountOutput.isSuccess()) {
+                    logger.info("退款单转抵异常，【退款编号:{},收款人:{},收款金额:{},msg:{}】", refundOrder.getCode(), o.getPayee(), o.getPayeeAmount(), accountOutput.getMessage());
+                    throw new BusinessException(ResultCode.DATA_ERROR, accountOutput.getMessage());
+                }
+            });
+        }
 
         return BaseOutput.success();
     }
--- src/main/java/com/dili/ia/service/impl/PassportRefundOrderServiceImpl.java
+++ src/main/java/com/dili/ia/service/impl/PassportRefundOrderServiceImpl.java
@@ -2,29 +2,43 @@ package com.dili.ia.service.impl;
 
 import com.dili.ia.domain.Passport;
 import com.dili.ia.domain.RefundOrder;
+import com.dili.ia.domain.TransferDeductionItem;
 import com.dili.ia.glossary.BizTypeEnum;
 import com.dili.ia.glossary.BoutiqueOrderStateEnum;
 import com.dili.ia.glossary.PassportStateEnum;
+import com.dili.ia.service.CustomerAccountService;
 import com.dili.ia.service.PassportService;
 import com.dili.ia.service.RefundOrderDispatcherService;
+import com.dili.ia.service.TransferDeductionItemService;
 import com.dili.settlement.domain.SettleOrder;
 import com.dili.ss.base.BaseServiceImpl;
 import com.dili.ss.constant.ResultCode;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.exception.BusinessException;
 import com.google.common.collect.Sets;
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 @Service
 public class PassportRefundOrderServiceImpl extends BaseServiceImpl<RefundOrder, Long> implements RefundOrderDispatcherService {
 
+    private final static Logger logger = LoggerFactory.getLogger(PassportRefundOrderServiceImpl.class);
+
     @Autowired
     private PassportService passportService;
 
+    @Autowired
+    private TransferDeductionItemService transferDeductionItemService;
+
+    @Autowired
+    private CustomerAccountService customerAccountService;
 
     /**
      * 退款单 -- 提交(无需改变通行证缴费单的状态)
@@ -67,6 +81,22 @@ public class PassportRefundOrderServiceImpl extends BaseServiceImpl<RefundOrder,
         }
 
         this.updateState(refundOrder.getBusinessCode(), passportInfo.getVersion(), PassportStateEnum.REFUNDED);
+
+        //转抵扣充值
+        TransferDeductionItem transferDeductionItemCondition = new TransferDeductionItem();
+        transferDeductionItemCondition.setRefundOrderId(refundOrder.getId());
+        List<TransferDeductionItem> transferDeductionItems = transferDeductionItemService.list(transferDeductionItemCondition);
+        if (CollectionUtils.isNotEmpty(transferDeductionItems)) {
+            transferDeductionItems.forEach(o -> {
+                BaseOutput accountOutput = customerAccountService.leaseOrderRechargTransfer(
+                        refundOrder.getId(), refundOrder.getCode(), o.getPayeeId(), o.getPayeeAmount(),
+                        refundOrder.getMarketId(), refundOrder.getRefundOperatorId(), refundOrder.getRefundOperator());
+                if (!accountOutput.isSuccess()) {
+                    logger.info("退款单转抵异常，【退款编号:{},收款人:{},收款金额:{},msg:{}】", refundOrder.getCode(), o.getPayee(), o.getPayeeAmount(), accountOutput.getMessage());
+                    throw new BusinessException(ResultCode.DATA_ERROR, accountOutput.getMessage());
+                }
+            });
+        }
 
         return BaseOutput.success();
     }
