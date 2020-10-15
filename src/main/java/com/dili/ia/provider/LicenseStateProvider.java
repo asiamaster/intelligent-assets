1891fbfedeaa44ebe397b1c0603d2dc78f2fda40
qinkelan <qinkelan@diligrp.com>
2020-10-15 17:51:28 +0800

    删除代码枚举删除了枚举：RefundTypeEnum  使用结算枚举：SettleWayEnum

--- src/main/java/com/dili/ia/provider/LicenseStateProvider.java
+++ src/main/java/com/dili/ia/provider/LicenseStateProvider.java
@@ -1,7 +1,6 @@
 package com.dili.ia.provider;
 
 import com.dili.ia.glossary.LicenseStateEnum;
-import com.dili.ia.glossary.RefundTypeEnum;
 import com.dili.ss.metadata.FieldMeta;
 import com.dili.ss.metadata.ValuePair;
 import com.dili.ss.metadata.ValuePairImpl;
