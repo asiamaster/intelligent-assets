57d918641a6571bf6bf3a95152d6f11533ee2d9a
qinkelan <qinkelan@diligrp.com>
2020-10-27 16:37:26 +0800

    作废脚本

---
+++ doc/20201112上线脚本-作废相关.sql
@@ -0,0 +1,11 @@
+ALTER TABLE `dili_ia`.`earnest_order`
+ADD COLUMN  `invalid_operator_id` bigint(20) DEFAULT NULL COMMENT '作废人ID' AFTER `canceler`,
+ADD COLUMN  `invalid_operator` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '作废人名字' AFTER `invalid_operator_id`,
+ADD COLUMN  `invalid_time` datetime DEFAULT NULL COMMENT '作废时间' AFTER `invalid_operator`,
+ADD COLUMN  `invalid_reason` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '作废原因' AFTER `invalid_time`;
+
+ALTER TABLE `dili_ia`.`deposit_order`
+ADD COLUMN  `invalid_operator_id` bigint(20) DEFAULT NULL COMMENT '作废人ID' AFTER `canceler`,
+ADD COLUMN  `invalid_operator` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '作废人名字' AFTER `invalid_operator_id`,
+ADD COLUMN  `invalid_time` datetime DEFAULT NULL COMMENT '作废时间' AFTER `invalid_operator`,
+ADD COLUMN  `invalid_reason` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '作废原因' AFTER `invalid_time`;
