d21fe58c1b5836f22f819c56949a6ae0c28b4d14
jiangchengyong <jiangchengyong@diligrp.com>
2020-11-24 16:26:58 +0800

    收款人 收款金额参数反了

--- src/main/java/com/dili/ia/service/impl/DepositOrderServiceImpl.java
+++ src/main/java/com/dili/ia/service/impl/DepositOrderServiceImpl.java
@@ -870,7 +870,7 @@ public class DepositOrderServiceImpl extends BaseServiceImpl<DepositOrder, Long>
         List<TransferDeductionItem> transferDeductionItems = transferDeductionItemService.list(transferDeductionItemCondition);
         if (CollectionUtils.isNotEmpty(transferDeductionItems)) {
             transferDeductionItems.forEach(o -> {
-                BaseOutput accountOutput = customerAccountService.rechargeTransferBalance(this.buildCustomerAccountParam(refundOrder, o.getPayeeId(), o.getPayeeAmount()));
+                BaseOutput accountOutput = customerAccountService.rechargeTransferBalance(this.buildCustomerAccountParam(refundOrder, o.getPayeeAmount(), o.getPayeeId()));
                 if (!accountOutput.isSuccess()) {
                     LOG.info("退款单转抵异常，【退款编号:{},收款人:{},收款金额:{},msg:{}】", refundOrder.getCode(), o.getPayee(), o.getPayeeAmount(), accountOutput.getMessage());
                     throw new BusinessException(ResultCode.DATA_ERROR, accountOutput.getMessage());
